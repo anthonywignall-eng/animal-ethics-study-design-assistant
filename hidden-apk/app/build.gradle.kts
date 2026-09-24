b3da3d7f7d0949cd7f6baf8b9c8c9af7a2a6cd9c
anthonywignall-eng <anthony.wignall@gmail.com>
2026-09-24 19:31:55 +0930

    Version HIDDEN 0.4.1

--- hidden-apk/app/build.gradle.kts
+++ hidden-apk/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.hidden.launcher"
         minSdk = 29
         targetSdk = 37
-        versionCode = 7
-        versionName = "0.4.0"
+        versionCode = 8
+        versionName = "0.4.1"
     }
 
     compileOptions {
